308b39e6ea57371c1609cc3d1ccd44bd92c97dfd
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-01-16 12:31:30 +0600

    Splash screen done

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,67 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:dropdown/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (_) => MyHomePage(title: "Home"),
+      ));
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.orange, Colors.yellow],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.fire_extinguisher_outlined,
+              size: 80,
+              color: Colors.red,
+            ),
+            Text(
+              "Fire Safety App",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 32,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
